d5854636d8f4eb71d00aa390015ee1b937efbbf9
Muhammad Ali <nerdjfpb@gmail.com>
2020-02-15 14:23:50 +0600

    Day 21

---
+++ Day 21/js/script.js
@@ -0,0 +1,13 @@
+console.log('Welcome to day 21')
+console.log('More Array')
+
+var favAnimeList = ['One piece', 'Dr Stone', 'Haikyuu', 'Attack on Titan']
+//delete the last one from array
+favAnimeList.pop()
+console.log(favAnimeList)
+//add a new item in array in last position
+favAnimeList.push('Naruto')
+console.log(favAnimeList)
+//sort the array
+favAnimeList.sort()
+console.log(favAnimeList)
